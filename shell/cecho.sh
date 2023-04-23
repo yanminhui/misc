3be0ca50e5ebfcf4799123444f57c011422282c4
Bryan Yan <yanminhui163@163.com>
2023-04-23 15:30:53 +0800

    cecho: raw text like echo if ANSI is not supported

--- shell/cecho.sh
+++ shell/cecho.sh
@@ -18,52 +18,53 @@ function sgrcolor() {
 
 # cecho - Write arguments to the standard output
 function cecho() {
+    local -ri TPUT_ENABLE=$(test -t 2 && which tput > /dev/null 2>&1 && echo 1 || echo 0)
     local OPTION OPTARG OPTIND
     while getopts ':nISWEBUCf:b:u:' OPTION; do
         case "$OPTION" in
-        n)
-            local -r DONT_APPEND_NEWLINE='\c'
-            ;;
-        I)
-            tput bold
-            tput setaf "$(sgrcolor cyan)"
-            ;;
-        S)
-            tput bold
-            tput setaf "$(sgrcolor green)"
-            ;;
-        W)
-            tput bold
-            tput setaf "$(sgrcolor yellow)"
-            ;;
-        E)
-            tput bold
-            tput setaf "$(sgrcolor red)"
-            ;;
-        B)
-            tput bold
-            ;;
-        U)
-            tput smul
-            ;;
-        C)
-            local -ri CLEAR_END_OF_LINE=1
-            ;;
-        f)
-            tput setaf "$(sgrcolor "$OPTARG")"
-            ;;
-        b)
-            tput setab "$(sgrcolor "$OPTARG")"
-            ;;
-        u)
-            local -r HYPERLINK="$OPTARG"
-            ;;
-        ?)
-            tput sgr0
-            cat >&2 << _EOF_
+            n)
+                local -r DONT_APPEND_NEWLINE='\c'
+                ;;
+            I)
+                local -ri SGR_BOLD=1
+                local -r SGR_FOREGROUND_COLOR='cyan'
+                ;;
+            S)
+                local -ri SGR_BOLD=1
+                local -r SGR_FOREGROUND_COLOR='green'
+                ;;
+            W)
+                local -ri SGR_BOLD=1
+                local -r SGR_FOREGROUND_COLOR='yellow'
+                ;;
+            E)
+                local -ri SGR_BOLD=1
+                local -r SGR_FOREGROUND_COLOR='red'
+                ;;
+            B)
+                local -ri SGR_BOLD=1
+                ;;
+            U)
+                local -ri SGR_UNDERLINE=1
+                ;;
+            C)
+                local -ri CLEAR_END_OF_LINE="$TPUT_ENABLE"
+                ;;
+            f)
+                local -r SGR_FOREGROUND_COLOR="$OPTARG"
+                ;;
+            b)
+                local -r SGR_BACKGROUND_COLOR="$OPTARG"
+                ;;
+            u)
+                local -r HYPERLINK="$OPTARG"
+                ;;
+            ?)
+                [[ "$TPUT_ENABLE" -eq 1 ]] && tput sgr0
+                cat >&2 << _EOF_
 $0: illegal option -- $OPTARG
 
-Usage: 
+Usage:
     cecho [-nISWEBUC] [-fb color] [-u url] [arg ...]
 
 Options:
@@ -83,25 +84,42 @@ Color:
     $(sgrcolor all)
 
 _EOF_
-            return 1
-            ;;
+                return 1
+                ;;
         esac
     done
     shift $((OPTIND - 1))
-
+    
+    # set graphics mode
+    if [[ $TPUT_ENABLE -eq 1 ]]; then
+        tput sgr "${SGR_STDOUT:-0}" \
+        "${SGR_UNDERLINE:-0}"       \
+        "${SGR_REVERSE:-0}"         \
+        "${SGR_BLINK:-0}"           \
+        "${SGR_DIM:-0}"             \
+        "${SGR_BOLD:-0}"            \
+        "${SGR_INVIS:-0}"           \
+        "${SGR_PROTECT:-0}"         \
+        "${SGR_ALTCHARSET:-0}"
+        
+        [[ -n "$SGR_BACKGROUND_COLOR" ]] && tput setab "$(sgrcolor "$SGR_BACKGROUND_COLOR")"
+        [[ -n "$SGR_FOREGROUND_COLOR" ]] && tput setaf "$(sgrcolor "$SGR_FOREGROUND_COLOR")"
+        
+        local -r SGR_RESET=$(tput sgr0)
+    fi
+    
     # for hyperlink
-    [[ -n "$CLEAR_END_OF_LINE" ]] && tput el
     if [[ -n "$HYPERLINK" ]]; then
-        printf "\e]8;;%s\a%s\e]8;;\a" "$HYPERLINK" "$*"
-        tput sgr0
+        [[ "$CLEAR_END_OF_LINE" -eq 1 ]] && tput el
+        printf "\e]8;;%s\a%s\e]8;;\a${SGR_RESET}" "$HYPERLINK" "$*"
         [[ -n "$DONT_APPEND_NEWLINE" ]] || printf "\n"
         return 0
     fi
-
+    
     if [[ -z "$(echo -e)" ]]; then
         local -r OPT_BACKSLASH_ESC='-e'
     fi
-
+    
     # for heredoc or file
     if [[ $# -eq 0 && ! -t 0 || -f $1 ]]; then
         local IFS=
@@ -112,19 +130,22 @@ _EOF_
             if [[ $CALL_ONCE -eq 0 ]]; then
                 CALL_ONCE=1
             else
-                [[ -n "$CLEAR_END_OF_LINE" ]] && tput el
+                [[ "$CLEAR_END_OF_LINE" -eq 1 ]] && tput el
                 echo $OPT_BACKSLASH_ESC "${PREV_LINE}${DONT_APPEND_NEWLINE}"
             fi
             PREV_LINE="$CURR_LINE"
         done < "${1:-/dev/stdin}"
-        [[ -n "$CLEAR_END_OF_LINE" ]] && tput el
-        echo $OPT_BACKSLASH_ESC "${PREV_LINE}$(tput sgr0)${DONT_APPEND_NEWLINE}"
+        [[ "$CLEAR_END_OF_LINE" -eq 1 ]] && tput el
+        echo $OPT_BACKSLASH_ESC "${PREV_LINE}${SGR_RESET}${DONT_APPEND_NEWLINE}"
         return 0
     fi
-
+    
     # for string
-    echo $OPT_BACKSLASH_ESC "$*$(tput sgr0)${DONT_APPEND_NEWLINE}"
+    [[ "$CLEAR_END_OF_LINE" -eq 1 ]] && tput el
+    echo $OPT_BACKSLASH_ESC "$*${SGR_RESET}${DONT_APPEND_NEWLINE}"
     return 0
 }
 
-cecho "$@"
+if [[ $# -gt 0 ]]; then
+    cecho "$@"
+fi
