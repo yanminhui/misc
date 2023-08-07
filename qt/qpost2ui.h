08301e8347c481a920bc253eae70898af3274174
Bryan Yan <yanminhui163@163.com>
2023-08-07 22:29:26 +0800

    qPost2Ui: improvment.

--- qt/qpost2ui.h
+++ qt/qpost2ui.h
@@ -1,52 +1,39 @@
 #pragma once
 
+#include <functional>
+
 #include <QCoreApplication>
-#include <QEvent>
+
+namespace detail {
+
+struct Poster : QObject
+{
+    using fn_t = std::function<void()>;
+
+    Q_OBJECT
+
+public slots:
+    void run(fn_t f)
+    {
+        f();
+    }
+};
+
+} // namespace detail
 
 template <typename F>
-void qPost2Ui(F f)
+void qPost2Ui(F&& f)
 {
-    class Event : public QEvent
-    {
-        using Fn = std::decay_t<F>;
+    static detail::Poster p{};
+    static auto _ = []() {
+        auto app = qApp;
+        Q_ASSERT(app);
 
-    public:
-        explicit Event(Fn fn)
-            : QEvent{QEvent::None}, fn_{fn}
-        {
-        }
+        auto ui_thrd = app->thread();
+        Q_ASSERT(ui_thrd);
 
-        ~Event() override
-        {
-            fn_();
-        }
-
-    private:
-        Fn fn_;
-    };
-
-    QCoreApplication::postEvent(qApp, new Event{std::forward<F>(f)});
+        p.moveToThread(ui_thrd);
+        return 0;
+    }(); // call once
+    QMetaObject::invokeMethod(&p, "run", Q_ARG(detail::Poster::fn_t, std::forward<F>(f)));
 }
-
-//#include <QThread>
-
-//#include "qpost2ui.h"
-
-//int main(int argc, char *argv[])
-//{
-//    QCoreApplication a(argc, argv);
-//    qDebug() << QThread::currentThreadId() << " main";
-//    std::async([](){
-//        qDebug() << QThread::currentThreadId() << " async";
-//        qPost2Ui([](){
-//            qDebug() << QThread::currentThreadId() << " lambda";
-//        });
-
-//    });
-//    return a.exec();
-//}
-
-// Result:
-// 0x1e5f3de00  main
-// 0x16b0fb000  async
-// 0x1e5f3de00  lambda
